d150df7ba6d7558316fbf22e381c71c61276b5ed
Francesco Lodolo <flod@lodolo.net>
2024-01-31 10:22:41 +0100

    Add script to extract Jira vendor stats

---
+++ scripts/jira_vendors_data.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import datetime
+import re
+from functions import get_jira_object, search_jira_issues
+
+
+def main():
+    jira = get_jira_object()
+
+    errors = []
+
+    issues = search_jira_issues(
+        jira,
+        f"project = 'l10n-vendor' AND status != Canceled AND issuetype != Epic",
+    )
+
+    output = []
+    for issue in issues:
+        issue_id = issue.key
+        date_created = datetime.datetime.strptime(
+            issue.fields.created, "%Y-%m-%dT%H:%M:%S.%f%z"
+        ).strftime("%Y-%m-%d")
+
+        # Try to extract numbers from the cost center field
+        original_cc = issue.fields.customfield_10450
+        if original_cc is None:
+            original_cc = ""
+        cc_numbers = re.findall(r"\d+", original_cc)
+        cc = "-"
+        if not cc_numbers:
+            errors.insert(
+                0, f"{issue_id}: cannot extract a cost center (value: {original_cc})"
+            )
+        elif len(cc_numbers) > 1:
+            errors.insert(
+                0,
+                f"{issue_id}: multiple numbers in cost center, first was used (value: {original_cc})",
+            )
+        else:
+            cc = cc_numbers[0]
+            if len(cc) < 4:
+                errors.insert(
+                    0,
+                    f"{issue_id}: extracted cost center ({cc}) is too short (value: {original_cc})",
+                )
+            elif len(cc) == 4:
+                cc = f"{cc}0"
+            cc = f"CC{cc}"
+
+        try:
+            type = issue.fields.parent.fields.summary
+        except AttributeError:
+            errors.insert(0, f"{issue_id}: no parent (epic) available")
+            type = "-"
+        output.append(f"{issue.key},{date_created},{issue.fields.reporter},{cc},{type}")
+
+    output.append("Issue ID,Date,Reporter,Cost Center,Epic")
+    output.reverse()
+
+    if errors:
+        print("There are errors:")
+        print("\n".join(errors))
+
+    print("\n----CSV output----\n\n")
+    print("\n".join(output))
+
+
+if __name__ == "__main__":
+    main()
