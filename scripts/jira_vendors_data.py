8d7b30d5d98e0545d851332b1d4657513a6a8df9
Francesco Lodolo <flod@lodolo.net>
2024-02-06 10:40:42 +0100

    Extract assignee for Jira vendor stats

--- scripts/jira_vendors_data.py
+++ scripts/jira_vendors_data.py
@@ -56,16 +56,18 @@ def main():
         if invoiced is None:
             invoiced = 0
 
+        assignee = issue.fields.assignee.displayName if issue.fields.assignee else "-"
+
         try:
             type = issue.fields.parent.fields.summary.strip()
         except AttributeError:
             errors.insert(0, f"{issue_id}: no parent (epic) available")
             type = "-"
         output.append(
-            f"{issue.key},{date_created},{issue.fields.reporter},{cc},{type},{invoiced}"
+            f"{issue.key},{date_created},{issue.fields.reporter},{cc},{type},{invoiced},{assignee}"
         )
 
-    output.append("Issue ID,Date,Reporter,Cost Center,Epic,Invoiced")
+    output.append("Issue ID,Date,Reporter,Cost Center,Epic,Invoiced,Assignee")
     output.reverse()
 
     if errors:
