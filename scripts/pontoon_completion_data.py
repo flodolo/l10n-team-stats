bd6bdc6b90b7849f1377399df25cb85f1e80658e
Francesco Lodolo <flod@lodolo.net>
2025-07-09 12:38:28 +0200

    Switch to Pontoon API v2

--- scripts/pontoon_completion_data.py
+++ scripts/pontoon_completion_data.py
@@ -4,9 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import json
-from urllib.parse import quote as urlquote
-from urllib.request import urlopen
+import requests
+import sys
 
 
 def main():
@@ -39,65 +38,54 @@ def main():
     ]
 
     # Get stats from Pontoon
-    query = """
-{
-    projects {
-        name
-        slug
-        localizations {
-            locale {
-                code
-            },
-            approvedStrings,
-            missingStrings,
-            pretranslatedStrings,
-            totalStrings
-        }
-    }
-}
-"""
-    locale_data = {}
-    try:
-        url = f"https://pontoon.mozilla.org/graphql?query={urlquote(query)}&raw"
-        response = urlopen(url)
-        json_data = json.load(response)
-
-        for project in json_data["data"]["projects"]:
-            slug = project["slug"]
-            if slug not in projects:
-                continue
-
-            for e in project["localizations"]:
-                locale = e["locale"]["code"]
-                if locale not in locale_data:
-                    locale_data[locale] = {
-                        "projects": 0,
-                        "missing": 0,
-                        "approved": 0,
-                        "pretranslated": 0,
-                        "total": 0,
-                        "completion": 0,
-                    }
-                locale_data[locale]["missing"] += e["missingStrings"]
-                locale_data[locale]["pretranslated"] += e["pretranslatedStrings"]
-                locale_data[locale]["approved"] += e["approvedStrings"]
-                locale_data[locale]["total"] += e["totalStrings"]
-                locale_data[locale]["projects"] += 1
-    except Exception as e:
-        print(e)
+    locale_stats = {}
+    for project in projects:
+        url = f"https://pontoon.mozilla.org/api/v2/projects/{project}"
+        page = 1
+        try:
+            while url:
+                print(f"Reading data for project {project} (page {page})")
+                response = requests.get(url)
+                response.raise_for_status()
+                data = response.json()
+                for locale, locale_data in data.get("localizations", {}).items():
+                    if locale not in top15:
+                        continue
+                    if locale not in locale_stats:
+                        locale_stats[locale] = {
+                            "projects": 0,
+                            "missing": 0,
+                            "approved": 0,
+                            "pretranslated": 0,
+                            "total": 0,
+                            "completion": 0,
+                        }
+                    locale_stats[locale]["missing"] += locale_data["missing_strings"]
+                    locale_stats[locale]["pretranslated"] += locale_data[
+                        "pretranslated_strings"
+                    ]
+                    locale_stats[locale]["approved"] += locale_data["approved_strings"]
+                    locale_stats[locale]["total"] += locale_data["total_strings"]
+                    locale_stats[locale]["projects"] += 1
+                # Get the next page URL
+                url = data.get("next")
+                page += 1
+        except requests.RequestException as e:
+            print(f"Error fetching data: {e}")
+            sys.exit()
 
     # Calculate average completion percentage
     total_strings = 0
     total_translations = 0
     for locale in top15:
-        total_strings += locale_data[locale]["total"]
-        total_translations += locale_data[locale]["approved"]
+        total_strings += locale_stats[locale]["total"]
+        total_translations += locale_stats[locale]["approved"]
     avg_completion = round((total_translations / total_strings) * 100, 2)
 
     print(f"Average completion level for top 15 locales: {avg_completion}%")
     # Using French as a proxy for number of strings
-    print(f"Number of strings (based on French): {locale_data['fr']['total']}")
-    print(f"Number of locales enabled in Pontoon: {len(locale_data)}")
+    print(f"Number of strings (based on French): {locale_stats['fr']['total']}")
+    print(f"Number of locales enabled in Pontoon: {len(locale_stats)}")
 
 
 if __name__ == "__main__":
